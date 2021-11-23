602925e09234584cf499319fb4d1152c613bcc18
Typicalesterest Guy (Элес) <zeyekereles@mail.ru>
2021-11-23 19:54:39 +0300

    ok

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import random as r
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Ui_MainWindow(QtWidgets.QWidget, object):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(816, 749)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(60, 510, 721, 221))
+        font = QtGui.QFont()
+        font.setFamily("Arial Black")
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "DO NOT PUSH THIS F*CKING BUTTON"))
+        self.pushButton.clicked.connect(self.do_some_fcking_magik)
+
+    def do_some_fcking_magik(self):
+        self.update()
+
+    def paintEvent(self, ev):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setBrush(QtGui.QColor(255, 255, 0))
+        qp.setPen(QtGui.QColor(0, 0, 0))
+        radius = r.randrange(0, 75)
+        qp.drawEllipse(150 - radius, 150 - radius, 150 + radius, 150 + radius)
+        qp.end()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Window1(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if True:
+        app = QtWidgets.QApplication(sys.argv)
+        app.setWindowIcon(QtGui.QIcon("icon.ico"))
+        ex = Window1()
+        ex.show()
+        sys.exit(app.exec_())
